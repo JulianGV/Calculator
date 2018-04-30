b4ddbb7eb5ccd2036849f4524956bff62dba99dd
JulianVerjan <averjan@gmail.com>
2018-04-30 02:33:18 -0500

    Calculator Exercise Sub Operation.
    Unit test class created and functionality tested.
    
    Testing done: Huawei P9 Lite
    
    Author: Julian Verjan julian.verjan@globant.com

--- app/src/main/java/com/app/calculator/Operator.java
+++ app/src/main/java/com/app/calculator/Operator.java
@@ -2,7 +2,10 @@ package com.app.calculator;
 
 public enum Operator {
     DEF("def","Select Operation"),
-    ADD("add","+");
+    ADD("add","+"),
+    MUL("multiplicate","*"),
+    SUB("subtract","-"),
+    DIV("divide","/");
 
     private String name, symbol;
 
